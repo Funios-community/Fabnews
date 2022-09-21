cbc5134430fa51e4a701aec9cbd887e73f0da1d0
Putri <putrichristina69@gmail.com>
2022-09-21 14:09:31 +0700

    feat(core): Add ListCommentNewsViewController xib

---
+++ Fabnews/Fabnews/Screen/DetailNews/DetailNewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailNewsViewController.swift
+//  Fabnews
+//
+//  Created by Hario Budiharjo on 12/09/22.
+//
+
+import UIKit
+
+class DetailNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+
---
+++ Fabnews/Fabnews/Screen/Login/LoginModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginModel.swift
+//  Fabnews
+//
+//  Created by Hario Budiharjo on 12/09/22.
+//
+
+import Foundation
