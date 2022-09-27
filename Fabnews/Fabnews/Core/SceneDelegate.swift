6d69a246c37abb7c90079e9dd9b621c00d7f2e9b
goeroeku <goeroeku@gmail.com>
2022-09-27 13:45:47 +0700

    Add main screen

--- Fabnews/Fabnews/Core/SceneDelegate.swift
+++ Fabnews/Fabnews/Core/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowsScene = (scene as? UIWindowScene) else { return }
-        let vc = DetailNewsViewController(nibName: String(describing: DetailNewsViewController.self), bundle: nil)
+//        let vc = DetailNewsViewController(nibName: String(describing: DetailNewsViewController.self), bundle: nil)
+        let vc = ListNewsViewController(nibName: String(describing: ListNewsViewController.self), bundle: nil)
         window = UIWindow(windowScene: windowsScene)
         window?.makeKeyAndVisible()
         window?.rootViewController = vc
